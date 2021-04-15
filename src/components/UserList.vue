f5bb5a8aeb4cfb411c3e5de911052c31fffaf852
Matías Romero <mromero.olivos@gmail.com>
2021-04-14 21:34:32 -0400

    TODO EL PROYECTO

---
+++ src/components/UserList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="row mx-0">
+    <div class="col-12 row mx-0">
+      <div class="col-md-6 col-lg-4 mx-auto mt-3"> 
+        <input type="text" class="form-control" placeholder="Buscar por nombre" v-model="filters.searchQuery" />
+      </div>
+      <div class="col-md-6 col-lg-4 mx-auto my-3"> 
+        <select v-model="filters.gender" class="form-control">
+          <option v-for="gender in filters.genderListObjects" :value="gender.value" :key="gender.value">
+            {{ gender.text }}
+          </option>
+        </select>
+      </div>
+      <div class="col-md-6 col-lg-4 mx-auto my-3"> 
+        <select v-model="filters.state" class="form-control">
+          <option v-for="state in filters.stateListObjects" v-bind:value="state.value" v-bind:key="state.value">
+            {{ state.text }}
+          </option>
+        </select>
+      </div>
+    </div>
+    <div v-for="user in filteredUsers" :key="user.id.value" class="col-12 col-md-6 col-lg-4 p-3">
+      <div class="border rounded py-2">
+        <img :src="user.picture.large" class="mb-3">
+        <h3>{{ `${user.name.first} ${user.name.last}` }}</h3>
+        <h6>{{ `${user.dob.age} años` }}</h6>
+        <h6>{{ `${user.location.state}` }}</h6>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  import { mapState, mapGetters } from "vuex"
+  
+  export default {
+    name: 'UserList',
+    computed: {
+      ...mapState(['filters']),
+      ...mapGetters(['filteredUsers'])
+    }
+  }
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,105 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import axios from 'axios'
+
+const instance = axios.create({
+	baseURL: "https://randomuser.me",
+	timeout: 1000,
+})
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    randomUsers: [],
+    filters: {
+      searchQuery: '',
+      gender: null,
+      genderList: [],
+      genderListObjects: Array({ text: 'Escoger género', value: null }),
+      state: null,
+      stateList: [],
+      stateListObjects: Array({ text: 'Escoger estado', value: null })
+    }
   },
   mutations: {
+    DATA_RANDOM_USERS(state, data) {
+      state.randomUsers = data
+    },
+    DATA_GENDERS(state, data) {
+      state.filters.genderList = data
+    },
+    DATA_GENDERS_OBJECTS(state, data) {
+      state.filters.genderListObjects = data
+    },
+    DATA_STATES(state, data) {
+      state.filters.stateList = data
+    },
+    DATA_STATES_OBJECTS(state, data) {
+      state.filters.stateListObjects = data
+    },
   },
   actions: {
+    actionGetRandomUsers({ commit }) {
+      const getRandomUsers = instance.get('/api/?results=100&nat=us')
+      getRandomUsers.then((res) => {
+        commit('DATA_RANDOM_USERS', res.data.results)
+
+        let genderList = []
+        let genderListObjects = []
+
+        let stateList = []
+        let stateListObjects = []
+        
+        res.data.results.forEach((user) => {
+          if (genderList.indexOf(user.gender) == -1) { genderList.push(user.gender); genderListObjects.push({ text: user.gender, value: user.gender }) }
+          if (stateList.indexOf(user.location.state) == -1) { stateList.push(user.location.state); stateListObjects.push({ text: user.location.state, value: user.location.state }) }
+
+        })
+
+        commit('DATA_GENDERS', genderList)
+        commit('DATA_GENDERS_OBJECTS', [...this.state.filters.genderListObjects, ...genderListObjects])
+
+        commit('DATA_STATES', stateList)
+        commit('DATA_STATES_OBJECTS', [...this.state.filters.stateListObjects, ...stateListObjects])
+
+      })
+    }
   },
-  modules: {
+  getters: {
+    filteredUsers: (state) => {
+      let users = state.randomUsers
+      let filteredUsers = []
+      let activeFilters = false
+
+      if (state.filters.searchQuery.length > 1) {
+        activeFilters = true
+        filteredUsers = users.filter(user => {
+          let userName = `${user.name.first} ${user.name.last}`.toLowerCase()
+          let inputText = state.filters.searchQuery.toLowerCase()
+          return userName.includes(inputText)
+        })
+      }
+
+      if (state.filters.gender != null) {
+        if (filteredUsers.length == 0 && activeFilters == false) filteredUsers  = users
+
+        activeFilters = true
+        filteredUsers = filteredUsers.filter(user => {
+          if (user.gender == state.filters.gender) return true
+        })
+      } 
+
+      if (state.filters.state != null) {
+        if (filteredUsers.length == 0 && activeFilters == false) filteredUsers  = users
+        activeFilters = true
+        filteredUsers = filteredUsers.filter(user => {
+          if (user.location.state == state.filters.state) return true
+        })
+      }
+      if (activeFilters) { return filteredUsers }
+      else { return users }
+    }
   }
 })
